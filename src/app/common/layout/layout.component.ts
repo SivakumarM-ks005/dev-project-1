d6683cfed648160ecba3119d8581aecca6b1005e
SivakumarM-ks005 <sivakumar.mani@kryasolutions.com>
2025-05-05 10:10:17 +0530

    initial
    initial

--- src/app/common/layout/layout.component.ts
+++ src/app/common/layout/layout.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-layout',
-  imports: [],
+  imports: [ FormsModule],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.scss'
 })
 export class LayoutComponent {
 
+ 
 }
--- src/login/login/login.component.ts
+++ src/login/login/login.component.ts
@@ -1,11 +1,33 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [ FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
 
+  loginObj={
+    "EmailId":"",
+      "Password": ""
+  };
+
+  http=inject(HttpClient);
+
+  login(){
+    debugger
+    this.http.post("https://freeapi.miniprojectideas.com/api/User/Login",this.loginObj).subscribe((res:any)=>{
+      debugger
+      if(res.result){
+        
+        alert("login success")
+      }else{
+        debugger
+        alert("check the username and password")
+      }
+    })
+  }
 }
