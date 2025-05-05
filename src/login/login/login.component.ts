0db8175e189309880619fd8c77b1b56684b60807
SivakumarM-ks005 <sivakumar.mani@kryasolutions.com>
2025-05-05 20:49:57 +0530

    login
    login

--- src/login/login/login.component.ts
+++ src/login/login/login.component.ts
@@ -1,5 +1,5 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, inject } from '@angular/core';
+import { Component, inject , OnInit} from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 @Component({
@@ -9,17 +9,23 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
-
+  result: any;
+constructor( private http: HttpClient){}
   loginObj={
     "EmailId":"",
       "Password": ""
   };
 
-  http=inject(HttpClient);
-
+  // http=inject(HttpClient);
+  ngOnInit(): void {
+      this.http.get("https://freeapi.miniprojectideas.com/api/User/GetAllUsers").subscribe( (data:any)=>{
+        this.result = data;
+        console.log("pring" , this.result.EmailId)
+      })
+}
   login(){
     debugger
-    this.http.post("https://freeapi.miniprojectideas.com/api/User/Login",this.loginObj).subscribe((res:any)=>{
+    this.http.post('https://freeapi.miniprojectideas.com/api/User/Login',this.loginObj).subscribe((res:any)=>{
       debugger
       if(res.result){
         
