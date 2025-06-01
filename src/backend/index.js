2cc000a39a09a5e4d2a1214cba5304b69832aad5
SivakumarM-ks005 <sivakumar.mani@kryasolutions.com>
2025-06-01 20:26:45 +0530

    Update index.js

--- src/backend/index.js
+++ src/backend/index.js
@@ -10,7 +10,7 @@ app.use(bodyParser.json());
 const dbConnection = mysql.createConnection({
    host: "localhost",
     port: 3306,
-    database: "devdb",
+    database: "devmysqldb",
     user: "root",
     password: "LogPwd@1310"
 });
@@ -22,8 +22,26 @@ dbConnection.connect(err=>{
 console.log('connection successfully')
     }
     
-})
+});
 
+// app.get('/user_details', (res, req)=>{
+//     console.log("postman is runing");
+//     let userList = `SELECT * FROM user_details`;
+//     dbConnection.query(userList, (err, results)=>{
+//         if(err){
+//             console.log(err, 'error');
+//         }
+//         if(results.length>0){
+//           res.send({
+//             data: results
+//           })
+//         }
+//     })
+// });
+
+app.get('/user_details', (req, res) => {
+  res.send('Hello, World!');
+});
 app.listen(3004,()=>{
     console.log('server is runing on port 3004');
 })
